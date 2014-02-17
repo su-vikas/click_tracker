22de292c4b64a87c51b49697690cea1471ec89a8
vikas <vikas@netskope.com>
2014-02-17 19:29:54 +0530

    not working with fileutils.jsm

---
+++ data/linksMap.js
@@ -0,0 +1,6 @@
+// object with attribute link and text is created and these are stored in an array to complete map for one activity. 
+
+function linkMap(link, text){
+    this.link = link;
+    this.text = text;
+}
