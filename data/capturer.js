2da972fdc070796f9436a7cb96d62abed5350cd4
vikas <vikas@netskope.com>
2014-02-21 16:46:17 +0530

    some commenting-uncommenting

--- data/capturer.js
+++ data/capturer.js
@@ -2,6 +2,7 @@ $('*').click(function(){
     var url = this.href;
     if(url!=undefined){
         var urlText = this.text;
+        //console.log(document.domain);
         //var str = "Href:" + url + "  text:" + urlText;
         node = [url,urlText]; 
         //console.log(node);
