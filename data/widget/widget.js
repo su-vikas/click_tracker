96066eb26d33b61bf1c036f8ec97573077915bf5
vikas <vikas@netskope.com>
2014-03-01 12:03:08 +0530

    added ctrl-click event, not used yet

--- data/widget/widget.js
+++ data/widget/widget.js
@@ -11,6 +11,9 @@ this.addEventListener('click', function(event) {
   if(event.button == 0 && event.shiftKey == false)
     self.port.emit('left-click');
 
+   if(event.button==0 && event.ctrlKey== true)
+        self.port.emit('ctrl-click');
+
   if(event.button == 2 || (event.button == 0 && event.shiftKey == true))
     self.port.emit('right-click');
     event.preventDefault();
