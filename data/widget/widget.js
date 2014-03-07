18f4bed452078985db2d3dbb2ecfdd820f64658e
vikas <vikas@netskope.com>
2014-03-07 16:49:59 +0530

    added notes, no major changes to widget.js

--- data/widget/widget.js
+++ data/widget/widget.js
@@ -8,10 +8,10 @@ to the content script.
 */
 
 this.addEventListener('click', function(event) {
-  if(event.button == 0 && event.shiftKey == false)
+  if(event.button == 0 && event.shiftKey == false && event.ctrlKey == false)
     self.port.emit('left-click');
 
-   if(event.button==0 && event.ctrlKey== true)
+   if(event.button == 0 && event.ctrlKey == true)
         self.port.emit('ctrl-click');
 
   if(event.button == 2 || (event.button == 0 && event.shiftKey == true))
