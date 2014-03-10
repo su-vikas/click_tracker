d953ca2acb61ca9dfde4dbac9644da8a96f29a45
vikas <vikas@netskope.com>
2014-03-10 12:01:26 +0530

    reading the file working, not tested with passing back the activity object

--- data/upload/file-upload.js
+++ data/upload/file-upload.js
@@ -1,99 +1,20 @@
-var activity_str = {"label":"dfdf", "domain":" ", "map":[{"url":"nitkkr.ac.in/pagesUI/homePage.xhtml?page=home&pageEvent=null&language=2","urlText":"Continue to main website"},{"url":"nitkkr.ac.in/pagesUI/homePage.jsf?pageEvent=4&page=content&language=2","urlText":"TRAINING & PLACEMENT"}]};
-
 function handleFileSelect(evt){
-
-    console.log("-------------------------------");
-    var activity  = JSON.parse(activity_str);
-    console.log(activity);
-    console.log(activity.label);
-    /*
-    var files=evt.target.files;
-
-    console.log(files);
-    console.log(evt);
-    //var file_name = $(this).val();
-    //var file_name = this.files[0];
-    console.log(file_name);
-    if(!file_name.type.match('application.json')){
-        console.log("not json file");
+    var files = evt.target.files;
+    console.log(JSON.stringify(files));
+    for(var i=0,f; f=files[i]; i++)
+    {
+        //TODO check if the file is JSON
+        var fr = new FileReader();
+        fr.onload = function(e){
+            var activity = JSON.parse(e.target.result);
+            console.log(JSON.stringify(activity));
+            alert(JSON.stringify(activity));
+        };
+        fr.readAsText(f);
     }
-    var fr = new FileReader();
-    fr.onload = function(e){
-        console.log("read event" );
-        var myJSON = JSON.parse(e.target.result);
-        console.log(myJSON);
-        alert(JSON.stringify(myJSON));
-    };
-    fr.readAsText(file_name);
-    */
 }
-document.getElementById('files').addEventListener('change', handleFileSelect,false);
-
-/*
-$('input[name=textjson]').change('input', function(data){
-    console.log("here");
-
-    activity_str = $(this).val();
-    var activity =JSON.parse(activity_str);
-    console.log(activity);
-    console.log(activity.label);
-});
-
-*/
-/*
-   $('input[name=uploadFile]').change(function(evt){
-    var files=evt.target.files;
-    console.log(evt);
-    console.log("-------------------------------");
-    console.log($(this));
-    var file_name = $(this).val();
-    //var file_name = this.files[0];
-    console.log(file_name);
-    if(!file_name.type.match('application.json')){
-        console.log("not json file");
-    }
-    var fr = new FileReader();
-    fr.onload = function(e){
-        console.log("read event" );
-        var myJSON = JSON.parse(e.target.result);
-        console.log(myJSON);
-        alert(JSON.stringify(myJSON));
-    };
-    fr.readAsText(file_name);
-    //var json = jQuery.parseJSON(file_name);
-    //console.log(json);
-    
-    /*
-    $.getJSON(file_name,function(data){
-        console.log("data:");
-        console.log(data);
-    });
-    
-});
-*/
 
 self.on('message', function onMessage(){
-    //console.log("show");
-    //var inputFile = document.getElementById('uploadFile').addEventListener('change', readSingleFile, false);
+    var f = document.getElementById('files').addEventListener('change', handleFileSelect,false);
 });
-function readSingleFile(evt){
-    console.log("entered");
-    var file = evt.target.files[0];
-    if(file){
-        console.log("entered into if");
-        var activity =jQuery.parseJSON(file);
-        console.log(activity);
-        /*
-           var reader = new FileReader();
-           reader.onload = function(e){
-           var contents = e.target.result;
-           var activity = eval(file);
 
-           };
-        reader.readAsText(file);
-        */
-    }
-    else{
-        console.log("Failed to load file");
-    }
-}
