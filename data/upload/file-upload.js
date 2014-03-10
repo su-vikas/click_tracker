df7ae6848b9fed4fcf2933019fdb19cdc1fd1b1d
vikas <vikas@netskope.com>
2014-03-10 12:32:42 +0530

    weird bug, after alert variable works fine, else not

--- data/upload/file-upload.js
+++ data/upload/file-upload.js
@@ -1,17 +1,23 @@
+var activity;
 function handleFileSelect(evt){
     var files = evt.target.files;
-    console.log(JSON.stringify(files));
+    //console.log(JSON.stringify(files));
     for(var i=0,f; f=files[i]; i++)
     {
-        //TODO check if the file is JSON
+        //console.log(i);
+        //TODO check if the file is JSON 
+        //TODO format is correct
         var fr = new FileReader();
         fr.onload = function(e){
-            var activity = JSON.parse(e.target.result);
-            console.log(JSON.stringify(activity));
-            alert(JSON.stringify(activity));
+            activity = JSON.parse(e.target.result);
+            //console.log(JSON.stringify(activity));
         };
         fr.readAsText(f);
     }
+    //send the captured activity object back to the main code. 
+    alert(JSON.stringify(activity));
+    console.log(JSON.stringify(activity));
+    self.port.emit('fileUploaded',activity);
 }
 
 self.on('message', function onMessage(){
