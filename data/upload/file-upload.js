74cb9c331299d74d1194bcf926c4c3c79c0ea4f7
vikas <vikas@netskope.com>
2014-03-11 11:25:24 +0530

    changes to test the code and make it working, still async read is an issue

--- data/upload/file-upload.js
+++ data/upload/file-upload.js
@@ -1,7 +1,7 @@
-var activity;
 function handleFileSelect(evt){
     var files = evt.target.files;
     //console.log(JSON.stringify(files));
+    var activity;
     for(var i=0,f; f=files[i]; i++)
     {
         //console.log(i);
@@ -15,8 +15,9 @@ function handleFileSelect(evt){
         fr.readAsText(f);
     }
     //send the captured activity object back to the main code. 
+    //TODO async file reading causing delay in reading file and further execution of code. shift to sync file reading 
     alert(JSON.stringify(activity));
-    console.log(JSON.stringify(activity));
+    //console.log(JSON.stringify(activity));
     self.port.emit('fileUploaded',activity);
 }
 
