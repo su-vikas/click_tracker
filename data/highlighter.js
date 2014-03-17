ce1a405ff3ea72978f3874f16ba941b7076424ff
vikas <vikas@netskope.com>
2014-03-17 14:27:33 +0530

    link matching and highliting working

--- data/highlighter.js
+++ data/highlighter.js
@@ -1,17 +1,46 @@
-/* This script is responsible for highlighting the links on the page, which are passed to this script 
-*/
-//TODO is there any best practice on where should computation to be perfomed, in content script or the main code of the add-on. 
+/*
+ * responsible for highlighting the links on the page
+ * Two approaches possible:
+ * 1. match the url. Cons: some site will have same url but diff url param. 
+ * 2. match the url text after partial matching url. TEST IT OUT. 
+ */
 
+/* gets the message and the links in form of activity object from add-on 
+ */
+//TODO deal when a new page is loaded 
 self.port.on('highlightLinks', function(activity){
 //get list of all links on the page
     console.log("in highlight links");
     console.log(activity);
+
+    // get all links on the page
     var linksOnPage = document.getElementsByTagName('a');
-    colorLinks(linksOnPage);
+    highlightLinks(activity, linksOnPage);
+   // colorLinks(linksOnPage);
 });
 
+//TODO check the present url. move to that point in the linkMap and then highlight
+// takes the activity object and array of links on the page and highlights 
+function highlightLinks(activity, linksOnPage){
+    console.log("yhaan hoon mai");
+    var linkMap = activity.map;
+    for(var i=0; i<linkMap.length; i++)
+    {
+        for(var j=0;j<linksOnPage.length; j++)
+        {
+            if(linkMap[i].urlText == linksOnPage[j].text)
+            {
+                //console.log("activity: " + linkMap[i].urlText + "dynamic: " + linksOnPage[j].text);
+                linksOnPage[j].style.color = 'red' ;
+            }
+        }
+    }
+}
+
+//color all links on the page. used while testing functionalities. 
 function colorLinks(linksArray){
     for (var i =0; i<linksArray.length;i++){
+        //console.log(linksArray[i].text);
         linksArray[i].style.color='red';
     }
 }
